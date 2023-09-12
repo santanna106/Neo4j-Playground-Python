95b435a8c340ee8d7541c9ca92549877a2924a16
Gabriel Andrade <gabriel.satnana@gmail.com>
2023-09-12 16:34:32 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,89 @@
+from neo4j import GraphDatabase
+
+
+uri = "neo4j://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "neo4j"))
+
+# CQL to query the distances from Yale to some of the other Ivy League universities
+
+cqlEdgeQuery          = "MATCH (x:university {name:'Yale University'})-[r]->(y:university) RETURN y.name,r.miles"
+
+cqlNodeQuery = "MATCH (x:university) RETURN x"
+
+# CQL to create a graph containing some of the Ivy League universities
+
+cqlCreate = """CREATE (cornell:university { name: "Cornell University"}),
+
+(yale:university { name: "Yale University"}),
+
+(princeton:university { name: "Princeton University"}),
+
+(harvard:university { name: "Harvard University"}),
+
+ 
+
+(cornell)-[:connects_in {miles: 259}]->(yale),
+
+(cornell)-[:connects_in {miles: 210}]->(princeton),
+
+(cornell)-[:connects_in {miles: 327}]->(harvard),
+
+ 
+
+(yale)-[:connects_in {miles: 259}]->(cornell),
+
+(yale)-[:connects_in {miles: 133}]->(princeton),
+(yale)-[:connects_in {miles: 133}]->(harvard),
+
+(harvard)-[:connects_in {miles: 327}]->(cornell),
+
+(harvard)-[:connects_in {miles: 133}]->(yale),
+
+(harvard)-[:connects_in {miles: 260}]->(princeton),
+
+(princeton)-[:connects_in {miles: 210}]->(cornell),
+
+(princeton)-[:connects_in {miles: 133}]->(yale),
+
+(princeton)-[:connects_in {miles: 260}]->(harvard)"""
+
+
+
+# Execute the CQL query
+
+with driver.session() as graphDB_Session:
+
+    # Create nodes
+
+    graphDB_Session.run(cqlCreate)
+
+   
+
+    # Query the graph    
+
+    nodes = graphDB_Session.run(cqlNodeQuery)
+
+   
+
+    print("List of Ivy League universities present in the graph:")
+
+    for node in nodes:
+
+        print(node)
+
+ 
+
+    # Query the relationships present in the graph
+
+    nodes = graphDB_Session.run(cqlEdgeQuery)
+
+   
+
+    print("Distance from Yale University to the other Ivy League universities present in the graph:")
+
+    for node in nodes:
+
+        print(node)
+
+
+driver.close()  # c
